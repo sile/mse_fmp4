62115a0faf0012a42547cd280b35beaa9678b5c2
Takeru Ohta <phjgt308@gmail.com>
2018-02-24 01:34:22 +0900

    Add `MoovBox` and `MvhdBox`

--- examples/parse.rs
+++ examples/parse.rs
@@ -1,40 +1,54 @@
+extern crate clap;
 extern crate fmp4;
 #[macro_use]
 extern crate trackable;
 
-use std::io::Read;
+use std::io::{stdin, Read};
+use clap::{App, Arg};
+use fmp4::{Error, ErrorKind, Result};
 use fmp4::isobmff;
-use trackable::error::Failure;
+
+macro_rules! track_io {
+    ($expr:expr) => {
+        $expr.map_err(|e: std::io::Error| {
+            use trackable::error::ErrorKindExt;
+            track!(Error::from(ErrorKind::Other.cause(e)))
+        })
+    }
+}
 
 fn main() {
+    let matches = App::new("parse")
+        .arg(
+            Arg::with_name("TYPE")
+                .long("type")
+                .takes_value(true)
+                .possible_values(&["toplevel", "tree"])
+                .default_value("toplevel"),
+        )
+        .get_matches();
+    match matches.value_of("TYPE").unwrap() {
+        "toplevel" => track_try_unwrap!(parse_toplevel()),
+        "tree" => track_try_unwrap!(parse_tree()),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_toplevel() -> Result<()> {
     let mut peek = [0];
-    while 1
-        == track_try_unwrap!(
-            std::io::stdin()
-                .read(&mut peek)
-                .map_err(Failure::from_error)
-        ) {
-        let header = track_try_unwrap!(fmp4::isobmff::BoxHeader::read_from(peek.chain(
-            std::io::stdin()
-        )));
+    while 1 == track_io!(stdin().read(&mut peek))? {
+        let header = track_try_unwrap!(isobmff::BoxHeader::read_from(peek.chain(stdin())));
         println!("{:?}", header);
         {
-            let mut reader = std::io::stdin().take(u64::from(header.data_size()));
-            match header.kind {
-                isobmff::FileTypeBox::TYPE => {
-                    let b = track_try_unwrap!(isobmff::FileTypeBox::read_from(&mut reader));
-                    println!("  {:?}", b);
-                }
-                isobmff::MediaDataBox::TYPE => {
-                    let b = track_try_unwrap!(isobmff::MediaDataBox::read_from(&mut reader));
-                    println!("  MediaDataBox({} bytes)", b.data.len());
-                }
-                _ => {
-                    let mut buf = Vec::new();
-                    track_try_unwrap!(reader.read_to_end(&mut buf).map_err(Failure::from_error));
-                }
-            }
-            assert_eq!(reader.limit(), 0);
+            let mut reader = stdin().take(u64::from(header.data_size()));
+            track_io!(reader.read_to_end(&mut Vec::new()))?;
         }
     }
+    Ok(())
+}
+
+fn parse_tree() -> Result<()> {
+    let file = track!(isobmff::File::read_from(stdin()));
+    println!("{:?}", file);
+    Ok(())
 }
--- src/isobmff.rs
+++ src/isobmff.rs
@@ -32,32 +32,15 @@ impl BoxHeader {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct FullBoxHeader {
-    pub size: u32,
-    pub kind: BoxType,
     pub version: u8,
     pub flags: u32, // u24
 }
 impl FullBoxHeader {
-    const SIZE: u32 = BoxHeader::SIZE + 4;
-
     pub fn read_from<R: Read>(mut reader: R) -> Result<Self> {
-        let base = track!(BoxHeader::read_from(&mut reader))?;
-        track_assert!(base.size >= Self::SIZE, ErrorKind::InvalidInput);
-
         let n = track_io!(reader.read_u32::<BigEndian>())?;
         let version = (n >> 24) as u8;
         let flags = n & 0xFF_FFFF;
-
-        Ok(FullBoxHeader {
-            size: base.size,
-            kind: base.kind,
-            version,
-            flags,
-        })
-    }
-
-    pub fn data_size(&self) -> u32 {
-        self.size - Self::SIZE
+        Ok(FullBoxHeader { version, flags })
     }
 }
 
@@ -153,5 +136,187 @@ impl MediaDataBox {
         let mut data = Vec::new();
         track_io!(reader.read_to_end(&mut data))?;
         Ok(MediaDataBox { data })
+    }
+}
+
+fn each_boxes<R: Read, F>(mut reader: R, mut f: F) -> Result<()>
+where
+    F: FnMut(BoxType, &mut ::std::io::Take<&mut R>) -> Result<()>,
+{
+    let mut peek = [0];
+    while 1 == track_io!(reader.read(&mut peek))? {
+        let header = track!(BoxHeader::read_from(peek.chain(&mut reader)))?;
+        let mut reader = reader.by_ref().take(u64::from(header.data_size()));
+        track!(f(header.kind, reader.by_ref()))?;
+        track_assert_eq!(reader.limit(), 0, ErrorKind::InvalidInput);
+    }
+    Ok(())
+}
+
+pub struct File {
+    pub ftyp_box: FileTypeBox,
+    pub moov_box: MoovBox,
+    pub mdat_boxes: Vec<MediaDataBox>,
+}
+impl File {
+    pub fn read_from<R: Read>(reader: R) -> Result<Self> {
+        let mut ftyp_box = None;
+        let mut moov_box = None;
+        let mut mdat_boxes = Vec::new();
+
+        track!(each_boxes(reader, |kind, reader| match &kind.0 {
+            b"ftyp" => {
+                track_assert!(ftyp_box.is_none(), ErrorKind::InvalidInput);
+                let x = track!(FileTypeBox::read_from(reader))?;
+                println!("[ftyp] {:?}", x);
+                ftyp_box = Some(x);
+                Ok(())
+            }
+            b"moov" => {
+                track_assert!(moov_box.is_none(), ErrorKind::InvalidInput);
+                println!("[moov]");
+                let x = track!(MoovBox::read_from(reader))?;
+                moov_box = Some(x);
+                Ok(())
+            }
+            b"mdat" => {
+                let x = track!(MediaDataBox::read_from(reader))?;
+                println!("[mdat] {} bytes", x.data.len());
+                mdat_boxes.push(x);
+                Ok(())
+            }
+            _ => {
+                println!("[todo] {:?}", kind);
+                track_io!(reader.read_to_end(&mut Vec::new()))?;
+                Ok(())
+            }
+        }))?;
+
+        let ftyp_box = track_assert_some!(ftyp_box, ErrorKind::InvalidInput);
+        let moov_box = track_assert_some!(moov_box, ErrorKind::InvalidInput);
+        Ok(File {
+            ftyp_box,
+            moov_box,
+            mdat_boxes,
+        })
+    }
+}
+impl fmt::Debug for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "File {{ ... }}")
+    }
+}
+
+/// Movie Box.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct MoovBox {
+    pub mvhd_box: MvhdBox,
+    pub trak_boxes: Vec<TrakBox>,
+}
+impl MoovBox {
+    pub fn read_from<R: Read>(reader: R) -> Result<Self> {
+        let mut mvhd_box = None;
+        let mut trak_boxes = Vec::new();
+
+        track!(each_boxes(reader, |kind, reader| match &kind.0 {
+            b"mvhd" => {
+                track_assert!(mvhd_box.is_none(), ErrorKind::InvalidInput);
+                let x = track!(MvhdBox::read_from(reader))?;
+                println!("    [mvhd] {:?}", x);
+                mvhd_box = Some(x);
+                Ok(())
+            }
+            b"trak" => {
+                println!("    [trak]");
+                let x = track!(TrakBox::read_from(reader))?;
+                trak_boxes.push(x);
+                Ok(())
+            }
+            _ => {
+                println!("    [todo] {:?}", kind);
+                track_io!(reader.read_to_end(&mut Vec::new()))?;
+                Ok(())
+            }
+        }))?;
+
+        let mvhd_box = track_assert_some!(mvhd_box, ErrorKind::InvalidInput);
+        track_assert!(!trak_boxes.is_empty(), ErrorKind::InvalidInput);
+        Ok(MoovBox {
+            mvhd_box,
+            trak_boxes,
+        })
+    }
+}
+
+/// Movie Header Box.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct MvhdBox {
+    pub creation_time: u64,
+    pub modification_time: u64,
+    pub timescale: u32,
+    pub duration: u64,
+    pub rate: i32,   // fixed point 16.16
+    pub volume: i16, // fixed point 8.8
+    pub matrix: [i32; 9],
+    pub next_track_id: u32, // 0xFFFF_FFFF means ...
+}
+impl MvhdBox {
+    pub fn read_from<R: Read>(mut reader: R) -> Result<Self> {
+        let header = track!(FullBoxHeader::read_from(&mut reader))?;
+
+        let creation_time;
+        let modification_time;
+        let timescale;
+        let duration;
+        if header.version == 0 {
+            creation_time = u64::from(track_io!(reader.read_u32::<BigEndian>())?);
+            modification_time = u64::from(track_io!(reader.read_u32::<BigEndian>())?);
+            timescale = track_io!(reader.read_u32::<BigEndian>())?;
+            duration = u64::from(track_io!(reader.read_u32::<BigEndian>())?);
+        } else if header.version == 1 {
+            creation_time = track_io!(reader.read_u64::<BigEndian>())?;
+            modification_time = track_io!(reader.read_u64::<BigEndian>())?;
+            timescale = track_io!(reader.read_u32::<BigEndian>())?;
+            duration = track_io!(reader.read_u64::<BigEndian>())?;
+        } else {
+            track_panic!(ErrorKind::Unsupported, "version={}", header.version);
+        }
+        let rate = track_io!(reader.read_i32::<BigEndian>())?;
+        let volume = track_io!(reader.read_i16::<BigEndian>())?;
+        let _ = track_io!(reader.read_u16::<BigEndian>())?; // reserved
+        let _ = track_io!(reader.read_u64::<BigEndian>())?; // reserved
+
+        let mut matrix = [0; 9];
+        for i in 0..9 {
+            matrix[i] = track_io!(reader.read_i32::<BigEndian>())?;
+        }
+        let _ = track_io!(reader.read_exact(&mut [0; 4 * 6]))?; // pre_defined
+        let next_track_id = track_io!(reader.read_u32::<BigEndian>())?;
+        Ok(MvhdBox {
+            creation_time,
+            modification_time,
+            timescale,
+            duration,
+            rate,
+            volume,
+            matrix,
+            next_track_id,
+        })
+    }
+}
+
+/// Track Box.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TrakBox {}
+impl TrakBox {
+    pub fn read_from<R: Read>(reader: R) -> Result<Self> {
+        track!(each_boxes(reader, |kind, reader| match &kind.0 {
+            _ => {
+                println!("        [todo] {:?}", kind);
+                track_io!(reader.read_to_end(&mut Vec::new()))?;
+                Ok(())
+            }
+        }))?;
+        Ok(TrakBox {})
     }
 }

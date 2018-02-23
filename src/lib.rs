4d03afa6498ac9ffcd499edc859a3b60f6298744
Takeru Ohta <phjgt308@gmail.com>
2018-02-23 16:09:04 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+extern crate byteorder;
+#[macro_use]
+extern crate trackable;
+
+pub use error::{Error, ErrorKind};
+
+mod error;
+
+/// This crate specific `Result` type.
+pub type Result<T> = std::result::Result<T, Error>;

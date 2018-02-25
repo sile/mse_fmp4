f42a0f0387075e47121cfd3416c8d281d2d07312
Takeru Ohta <phjgt308@gmail.com>
2018-02-25 17:28:39 +0900

    Add `MovieFragmentBox`, `MovieFragmentHeaderBox`, `TrackFragmentBox`

--- src/fmp4.rs
+++ src/fmp4.rs
@@ -20,9 +20,9 @@ macro_rules! write_u32 {
 macro_rules! write_i32 {
     ($w:expr, $n:expr) => { track_io!($w.write_i32::<BigEndian>($n))?; }
 }
-// macro_rules! write_u64 {
-//     ($w:expr, $n:expr) => { track_io!($w.write_u64::<BigEndian>($n))?; }
-// }
+macro_rules! write_u64 {
+    ($w:expr, $n:expr) => { track_io!($w.write_u64::<BigEndian>($n))?; }
+}
 macro_rules! write_all {
     ($w:expr, $n:expr) => { track_io!($w.write_all($n))?; }
 }
@@ -100,7 +100,7 @@ pub struct File {
     pub ftyp_box: FileTypeBox,
     pub moov_box: MovieBox,
     pub mdat_boxes: Vec<MediaDataBox>,
-    // pub moof_boxes: Vec<MoofBox>,
+    pub moof_boxes: Vec<MovieFragmentBox>,
     pub mfra_box: MovieFragmentRandomAccessBox,
 }
 impl File {
@@ -109,6 +109,7 @@ impl File {
             ftyp_box: FileTypeBox::default(),
             moov_box: MovieBox::new(),
             mdat_boxes: Vec::new(),
+            moof_boxes: Vec::new(),
             mfra_box: MovieFragmentRandomAccessBox::new(),
         }
     }
@@ -118,6 +119,7 @@ impl WriteTo for File {
         write_box!(writer, self.ftyp_box);
         write_box!(writer, self.moov_box);
         write_boxes!(writer, &self.mdat_boxes);
+        write_boxes!(writer, &self.moof_boxes);
         write_box!(writer, self.mfra_box);
         Ok(())
     }
@@ -197,6 +199,212 @@ impl WriteTo for MovieBox {
         write_box!(writer, self.mvhd_box);
         write_boxes!(writer, &self.trak_boxes);
         write_box!(writer, self.mvex_box);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct MovieFragmentBox {
+    pub mfhd_box: MovieFragmentHeaderBox,
+    pub traf_box: TrackFragmentBox,
+}
+impl MovieFragmentBox {
+    pub fn new(track_id: u32) -> Self {
+        MovieFragmentBox {
+            mfhd_box: MovieFragmentHeaderBox::new(),
+            traf_box: TrackFragmentBox::new(track_id),
+        }
+    }
+}
+impl WriteBoxTo for MovieFragmentBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"moof")
+    }
+}
+impl WriteTo for MovieFragmentBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        write_box!(writer, self.mfhd_box);
+        write_box!(writer, self.traf_box);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct TrackFragmentBox {
+    pub tfhd_box: TrackFragmentHeaderBox,
+    pub tfdt_box: TrackFragmentBaseMediaDecodeTimeBox,
+    pub trun_box: TrackRunBox,
+}
+impl TrackFragmentBox {
+    pub fn new(track_id: u32) -> Self {
+        TrackFragmentBox {
+            tfhd_box: TrackFragmentHeaderBox::new(track_id),
+            tfdt_box: TrackFragmentBaseMediaDecodeTimeBox::new(),
+            trun_box: TrackRunBox::new(),
+        }
+    }
+}
+impl WriteBoxTo for TrackFragmentBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"traf")
+    }
+}
+impl WriteTo for TrackFragmentBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        write_box!(writer, self.tfhd_box);
+        write_box!(writer, self.tfdt_box);
+        write_box!(writer, self.trun_box);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct MovieFragmentHeaderBox {
+    pub sequence_number: u32,
+}
+impl MovieFragmentHeaderBox {
+    pub fn new() -> Self {
+        MovieFragmentHeaderBox { sequence_number: 0 }
+    }
+}
+impl WriteBoxTo for MovieFragmentHeaderBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"mfhd")
+    }
+    fn full_box_header(&self) -> Option<FullBoxHeader> {
+        Some(FullBoxHeader::new(0, 0))
+    }
+}
+impl WriteTo for MovieFragmentHeaderBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        track_assert_ne!(self.sequence_number, 0, ErrorKind::InvalidInput);
+        write_u32!(writer, self.sequence_number);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct TrackRunBox {
+    pub data_offset: Option<i32>,
+    pub first_sample_flags: Option<u32>,
+    pub entries: Vec<TrunEntry>,
+}
+impl TrackRunBox {
+    pub fn new() -> Self {
+        TrackRunBox {
+            data_offset: None,
+            first_sample_flags: None,
+            entries: Vec::new(),
+        }
+    }
+}
+impl WriteBoxTo for TrackRunBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"trun")
+    }
+    fn full_box_header(&self) -> Option<FullBoxHeader> {
+        let flags = unimplemented!();
+        Some(FullBoxHeader::new(0, flags))
+    }
+}
+impl WriteTo for TrackRunBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TrunEntry {
+    pub sample_duration: Option<u32>,
+    pub sample_size: Option<u32>,
+    pub sample_flags: Option<u32>,
+    pub sample_composition_time_offset: Option<i64>,
+}
+
+#[derive(Debug)]
+pub struct TrackFragmentBaseMediaDecodeTimeBox {
+    pub base_media_decode_time: u32,
+}
+impl TrackFragmentBaseMediaDecodeTimeBox {
+    pub fn new() -> Self {
+        TrackFragmentBaseMediaDecodeTimeBox {
+            base_media_decode_time: 0,
+        }
+    }
+}
+impl WriteBoxTo for TrackFragmentBaseMediaDecodeTimeBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"tfdt")
+    }
+    fn full_box_header(&self) -> Option<FullBoxHeader> {
+        Some(FullBoxHeader::new(0, 0))
+    }
+}
+impl WriteTo for TrackFragmentBaseMediaDecodeTimeBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        write_u32!(writer, self.base_media_decode_time);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct TrackFragmentHeaderBox {
+    pub track_id: u32,
+    pub duration_is_empty: bool,
+    pub default_base_is_moof: bool,
+    pub base_data_offset: Option<u64>,
+    pub sample_description_index: Option<u32>,
+    pub default_sample_duration: Option<u32>,
+    pub default_sample_size: Option<u32>,
+    pub default_sample_flags: Option<u32>,
+}
+impl TrackFragmentHeaderBox {
+    pub fn new(track_id: u32) -> Self {
+        TrackFragmentHeaderBox {
+            track_id,
+            duration_is_empty: false,
+            default_base_is_moof: false,
+            base_data_offset: None,
+            sample_description_index: None,
+            default_sample_duration: None,
+            default_sample_size: None,
+            default_sample_flags: None,
+        }
+    }
+}
+impl WriteBoxTo for TrackFragmentHeaderBox {
+    fn box_type(&self) -> BoxType {
+        BoxType(*b"tfhd")
+    }
+    fn full_box_header(&self) -> Option<FullBoxHeader> {
+        let flags = (self.base_data_offset.is_some() as u32 * 0x00_0001)
+            | (self.sample_description_index.is_some() as u32 * 0x00_0002)
+            | (self.default_sample_duration.is_some() as u32 * 0x00_0008)
+            | (self.default_sample_size.is_some() as u32 * 0x00_0010)
+            | (self.default_sample_flags.is_some() as u32 * 0x00_0020)
+            | (self.duration_is_empty as u32 * 0x01_0000)
+            | (self.default_base_is_moof as u32 * 0x02_0000);
+        Some(FullBoxHeader::new(0, flags))
+    }
+}
+impl WriteTo for TrackFragmentHeaderBox {
+    fn write_to<W: Write>(&self, mut writer: W) -> Result<()> {
+        write_u32!(writer, self.track_id);
+        if let Some(x) = self.base_data_offset {
+            write_u64!(writer, x);
+        }
+        if let Some(x) = self.sample_description_index {
+            write_u32!(writer, x);
+        }
+        if let Some(x) = self.default_sample_duration {
+            write_u32!(writer, x);
+        }
+        if let Some(x) = self.default_sample_size {
+            write_u32!(writer, x);
+        }
+        if let Some(x) = self.default_sample_flags {
+            write_u32!(writer, x);
+        }
         Ok(())
     }
 }
